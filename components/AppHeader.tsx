db79b98151d57b77cbe4c18d91d71de9bae8d555
Md-E7 <madeegi7@gmail.com>
2023-02-24 10:26:18 +0800

    :iphone: Add responsive design

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -10,39 +10,39 @@ const AppHeader = (): JSX.Element => {
   }
 
   return (
-    <header className={'sticky top-0 flex items-center justify-between p-4'}>
-      <h1 className={'text-3xl font-bold'}>Md-E7</h1>
+    <header className={'sticky top-0 flex items-center justify-between p-4 md:px-6 lg:px-10 xl:px-12'}>
+      <h1 className={'text-3xl font-bold sm:text-4xl'}>Md-E7</h1>
 
-      <div className={'flex items-center gap-2'}>
-        <button onClick={toggleTheme} className="rounded-md p-1">
+      <div className={'flex items-center gap-2 lg:hidden'}>
+        <button onClick={toggleTheme} className={'rounded-lg p-1'}>
           {theme === 'light' && (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
               <title>Light</title>
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
             </svg>
           )}
 
           {theme === 'dark' && (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
               <title>Dark</title>
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"/>
             </svg>
           )}
 
           {theme === 'system' && (
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
               <title>System</title>
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"/>
             </svg>
           )}
         </button>
 
-        <button onClick={toggleMobileMenu}>
+        <button onClick={toggleMobileMenu} className={'rounded-lg p-1'}>
           {isMobileMenuOpen
-            ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8">
+            ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
             </svg>
-            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8">
+            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
             </svg>
           }
@@ -50,11 +50,42 @@ const AppHeader = (): JSX.Element => {
 
         {isMobileMenuOpen &&
           <nav className={'absolute right-5 top-0 flex translate-y-1/2 flex-col rounded border border-slate-300 bg-white shadow'}>
-            <a href="#about_me" onClick={closeMobileMenu} className={'py-1.5 px-3 font-medium'}>Tentang saya</a>
-            <a href="#my_skills" onClick={closeMobileMenu} className={'py-1.5 px-3 font-medium'}>Kehalian saya</a>
-            <a href="#my_projects" onClick={closeMobileMenu} className={'py-1.5 px-3 font-medium'}>Proyek saya</a>
+            <a href="#about_me" onClick={closeMobileMenu} className={'py-1.5 px-3 font-medium sm:text-lg'}>Tentang saya</a>
+            <a href="#my_skills" onClick={closeMobileMenu} className={'py-1.5 px-3 font-medium sm:text-lg'}>Kehalian saya</a>
+            <a href="#my_projects" onClick={closeMobileMenu} className={'py-1.5 px-3 font-medium sm:text-lg'}>Proyek saya</a>
           </nav>
         }
+      </div>
+
+      <div className={'hidden items-center gap-4 lg:flex'}>
+        <nav className={'flex gap-2'}>
+          <a href="#about_me" onClick={closeMobileMenu} className={'py-1.5 px-2 font-medium sm:text-lg'}>Tentang saya</a>
+          <a href="#my_skills" onClick={closeMobileMenu} className={'py-1.5 px-2 font-medium sm:text-lg'}>Kehalian saya</a>
+          <a href="#my_projects" onClick={closeMobileMenu} className={'py-1.5 px-2 font-medium sm:text-lg'}>Proyek saya</a>
+        </nav>
+
+        <button onClick={toggleTheme} className={'rounded-lg p-1'}>
+          {theme === 'light' && (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
+              <title>Light</title>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
+            </svg>
+          )}
+
+          {theme === 'dark' && (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
+              <title>Dark</title>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"/>
+            </svg>
+          )}
+
+          {theme === 'system' && (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 sm:h-8 sm:w-8">
+              <title>System</title>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"/>
+            </svg>
+          )}
+        </button>
       </div>
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,16 +21,18 @@ const Index = ({ repos }: { repos: Repository[] }): JSX.Element => {
       <AppHeader/>
 
       <main className="flex flex-col items-center justify-center">
-        <section id={'about_me'} className={'flex min-h-screen w-full max-w-sm flex-col justify-center gap-6 p-4'}>
-          <Image src={'/images/md-e7.svg'} alt={'Md-E7 logo'} width={500} height={500} className="mx-auto w-44 rounded-full border border-slate-300"/>
+        <section id={'about_me'} className={'flex min-h-screen w-full max-w-sm flex-col justify-center gap-6 p-4 sm:max-w-xl lg:max-w-4xl lg:flex-row-reverse lg:items-center xl:max-w-5xl'}>
+          <Image src={'/images/md-e7.svg'} alt={'Md-E7 logo'} width={500} height={500} className="m-auto w-44 rounded-full border border-slate-300 sm:w-52 lg:w-64"/>
 
-          <div className={'flex flex-col gap-4 text-center'}>
-            <h2 className={'space-x-2 text-2xl font-bold'}>
-              <span>Made Egi Darma Saputra Front-End</span>
-              <span className={'text-blue-600'}>Developer</span>
+          <div className={'flex flex-col gap-4 text-center lg:text-start'}>
+            <h2 className={'text-2xl font-bold sm:text-3xl'}>
+              <span>Made Egi Darma Saputra</span>
+              <br/>
+              <span>Front-End</span>
+              <span className={'ml-2 text-blue-600'}>Developer</span>
             </h2>
 
-            <div className={'flex flex-col gap-2'}>
+            <div className={'flex flex-col gap-2 sm:text-lg lg:max-w-lg'}>
               <p className={'space-x-1 text-slate-500'}>
                 <span className={'font-medium'}>Halo 👋</span>
                 <span>Saya Egi, saya ingin menjadi Full Stack Developer.</span>
@@ -39,7 +41,7 @@ const Index = ({ repos }: { repos: Repository[] }): JSX.Element => {
               <p className={'text-slate-500'}>Saya kurang bagus dalam mendesain tampilan jadi saya biasanya mencari referensi dari beberapa website dan menggabungkan referensi yang saya dapat dan mulai membuat tampilan.</p>
             </div>
 
-            <div className={'flex items-center justify-center gap-2'}>
+            <div className={'flex items-center justify-center gap-2 lg:justify-start'}>
               <a href="https://github.com/md-e7">
                 <Image src={'/images/github.svg'} alt={'Github Logo'} width={32} height={32} className={'h-8 w-8'}/>
               </a>
@@ -47,77 +49,77 @@ const Index = ({ repos }: { repos: Repository[] }): JSX.Element => {
           </div>
         </section>
 
-        <section id={'my_skills'} className={'flex min-h-screen w-full max-w-sm flex-col justify-center gap-6 p-4'}>
-          <div className={'flex flex-col gap-4 text-center'}>
-            <h2 className={'text-2xl font-bold'}>Kehalian saya</h2>
-            <p className={'text-slate-500'}>Ini semua adalah bahasa program, bahasa markah, kerangka kerja, alat, yang biasa saya gunakan dan sudah luamayn saya pahami.</p>
+        <section id={'my_skills'} className={'flex min-h-screen w-full max-w-sm flex-col justify-center gap-6 p-4 sm:max-w-xl lg:max-w-4xl xl:max-w-5xl'}>
+          <div className={'mx-auto flex flex-col gap-4 text-center lg:max-w-xl'}>
+            <h2 className={'text-2xl font-bold sm:text-3xl'}>Kehalian saya</h2>
+            <p className={'text-slate-500 sm:text-lg'}>Ini semua adalah bahasa program, bahasa markah, kerangka kerja, alat, yang biasa saya gunakan dan sudah luamayn saya pahami.</p>
           </div>
 
-          <div className={'grid grid-cols-2 items-center justify-items-center gap-2'}>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+          <div className={'grid grid-cols-2 items-center justify-items-center gap-2 sm:grid-cols-3 xl:grid-cols-4'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/html5.svg'} alt={'HTML5 Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>HTML5</span>
+              <span className={'font-medium lg:text-lg'}>HTML5</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/css3.svg'} alt={'CSS 3 Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>CSS 3</span>
+              <span className={'font-medium lg:text-lg'}>CSS 3</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/javascript.svg'} alt={'JavaScript Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>JavaScript</span>
+              <span className={'font-medium lg:text-lg'}>JavaScript</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/nodejs.svg'} alt={'Node.js Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>Node.js</span>
+              <span className={'font-medium lg:text-lg'}>Node.js</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/typescript.svg'} alt={'TypeScript Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>TypeScript</span>
+              <span className={'font-medium lg:text-lg'}>TypeScript</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/eslint.svg'} alt={'ESLint Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>ESLint</span>
+              <span className={'font-medium lg:text-lg'}>ESLint</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/tailwindcss.svg'} alt={'Tailwind CSS Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>Tailwind CSS</span>
+              <span className={'font-medium lg:text-lg'}>Tailwind CSS</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/nextjs.svg'} alt={'Next.js Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>Next.js</span>
+              <span className={'font-medium lg:text-lg'}>Next.js</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/koajs.svg'} alt={'Koa.js Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>Koa.js</span>
+              <span className={'font-medium lg:text-lg'}>Koa.js</span>
             </div>
-            <div className={'flex w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
+            <div className={'flex h-full w-full max-w-sm items-center justify-center gap-4 rounded bg-white p-2 shadow'}>
               <Image src={'/images/expressjs.svg'} alt={'Express.js Logo'} width={32} height={32} className={'h-10 w-10'}/>
-              <span className={'font-medium'}>Express.js</span>
+              <span className={'font-medium lg:text-lg'}>Express.js</span>
             </div>
           </div>
         </section>
 
-        <section id={'my_projects'} className={'flex min-h-screen w-full max-w-sm flex-col justify-center gap-6 p-4'}>
-          <div className={'flex flex-col gap-4 text-center'}>
-            <h2 className={'text-2xl font-bold'}>Proyek saya</h2>
+        <section id={'my_projects'} className={'flex min-h-screen w-full max-w-sm flex-col justify-center gap-6 p-4 sm:max-w-xl lg:max-w-4xl xl:max-w-5xl'}>
+          <div className={'mx-auto flex flex-col gap-4 text-center lg:max-w-xl'}>
+            <h2 className={'text-2xl font-bold sm:text-3xl'}>Proyek saya</h2>
 
             {repos.length === 0 && (
-              <p className={'text-slate-500'}>Belum ada proyek.</p>
+              <p className={'text-slate-500 sm:text-lg'}>Belum ada proyek.</p>
             )}
 
             {repos.length === 1 && (
-              <p className={'text-slate-500'}>Ini adalah proyek yang saya buat.</p>
+              <p className={'text-slate-500 sm:text-lg'}>Ini adalah proyek yang saya buat.</p>
             )}
 
             {repos.length > 1 && (
-              <p className={'text-slate-500'}>Ini semua adalah proyek yang saya buat.</p>
+              <p className={'text-slate-500 sm:text-lg'}>Ini semua adalah proyek yang saya buat.</p>
             )}
           </div>
 
-          <div className={'grid grid-cols-1 gap-2'}>
+          <div className={'grid grid-cols-1 gap-2 sm:grid-cols-2 xl:grid-cols-3'}>
             {repos.map(value => (
               <a key={value.id} href={value.html_url} className={'group flex w-full flex-col gap-1 rounded bg-white p-4 shadow'}>
-                <h3 className={'text-xl font-semibold group-hover:text-blue-500'}>{value.name}</h3>
+                <h3 className={'truncate text-xl font-semibold group-hover:text-blue-500'}>{value.name}</h3>
                 <p className={'text-sm text-slate-500'}>{value.description ?? 'No description'}</p>
                 <div className={'flex justify-between'}>
                   <p className={'text-sm text-slate-500'}>{value.language}</p>
